f439a39f8b9e28957afc623bc6badec18fc47a6a
Sean Haddock <seanhaddock@live.com>
2021-06-25 14:14:17 -0500

    wip

---
+++ CarvedRock.API/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using CarvedRock.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarvedRock.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = new List<Product>()
+            { new Product() { Id = 1, Name = "Product 1" } };
+
+            return Ok(products);
+        }
+    }
+}
